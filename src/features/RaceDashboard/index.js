a1432c53f3122ce7a3393c9841aab5c7a8d895bf
cevazquez <carlvazquez498@gmail.com>
2019-04-11 03:32:57 -0600

    Add Dashboard and NavBar components

---
+++ src/features/RaceDashboard/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class RaceDashboard extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div class="row">
+          <div class="col-7"><h2>Race List Here</h2></div>
+          <div class="col-5"><h3>Recent Activity here</h3></div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default RaceDashboard;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -5,6 +9,10 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+ 
+if (module.hot) {
+  module.hot.accept();
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
